02449261ca98086edaa39287df5b5d889496a0c8
jedsada <jt@20scoops.com>
2017-04-10 23:08:31 +0700

    changed APIs for test.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,14 +18,22 @@ export class SignupComponent {
   constructor(public http: Http) {}
 
   signup() {
-    const data = {
-      user: 'paradon',
-      pass: 'paradon@1'
-    };
-    const myHeaders = new Headers({
-      'Content-Type': 'application/json'
-    });
-    this.http.get('https://api.github.com/users/t-jedsada')
+    // const data = {
+    //   user: 'paradon',
+    //   pass: 'paradon@1'
+    // };
+    // const myHeaders = new Headers({
+    //   'Content-Type': 'application/json'
+    // });
+    let params: URLSearchParams = new URLSearchParams();
+    params.set('apikey', '274d2a4a98a5d12a240fed5cf66733ac');
+    params.set('code', 'lrcmb');
+    params.set('action', 'user_login');
+    params.set('username', 'admin');
+    params.set('password', '1112');
+    this.http.get('http://sc2lclinic.com/backend/system/api/json/main.php', {
+        search: params
+      })
       .subscribe(
         response => {
           console.log(response);
